e3d1b27213d3969e8bc785ba1e457ba4b31e6df0
GeneTv <lpgenehd@gmail.com>
2020-04-01 14:43:47 +0200

    Added particles, navbar transition + title, content container & fixed wrapper

--- assets/handler.js
+++ assets/handler.js
@@ -1,5 +1,18 @@
 async function init() {
   await $('#navigation').load('./components/Navbar.html')
   await $('#footer').load('./components/Footer.html')
+
+  const navigation = $('#navigation')
+  var positionScroll = $(window).scrollTop()
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop()
+    if (scroll > positionScroll) {
+      navigation.addClass('fadeout')
+    } else {
+      navigation.removeClass('fadeout')
+    }
+    positionScroll = scroll;
+  });
+  particlesJS.load('particles-js', '/assets/particles.json');
 }
 init()

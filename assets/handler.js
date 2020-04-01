bdb2606a1dead8f47e53c828cc631969a9cfacfe
GeneTv <lpgenehd@gmail.com>
2020-04-01 11:24:38 +0200

    Update handler.js
    Removed console logs from ressource requests

--- assets/handler.js
+++ assets/handler.js
@@ -1,10 +1,5 @@
 async function init() {
-  await $('#navigation').load('./components/Navbar.html', () => {
-    console.log('Navigation loaded')
-  })
-
-  await $('#footer').load('./components/Footer.html', () => {
-    console.log('Navigation loaded')
-  })
+  await $('#navigation').load('./components/Navbar.html')
+  await $('#footer').load('./components/Footer.html')
 }
 init()
